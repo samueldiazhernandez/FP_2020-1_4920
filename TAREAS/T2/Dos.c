94f9129411b2d24fa41444c3671eabed256a6f0e
Samuel <samuel@iMac-de-mac.local>
2019-10-07 19:03:16 -0500

    Las dos tareas

---
+++ TAREAS/T2/Dos.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int int main(int argc, char const *argv[])
+{
+	int numero;
+	numero=atoi(argu[1]);
+	if (numero%2==0){
+		printf("1\n");
+	}
+	else {
+		printf("0\n");
+	}
+	return 0;
+}
